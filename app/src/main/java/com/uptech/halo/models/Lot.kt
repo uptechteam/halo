e94f55999429245099e727f1372c263323c2a881
Rodion Kushch <rodionkushch@Rodions-MacBook-Pro.local>
2022-09-17 16:36:04 +0300

    move packages

--- app/src/main/java/com/uptech/halo/models/Lot.kt
+++ app/src/main/java/com/uptech/halo/models/Lot.kt
@@ -8,20 +8,3 @@ data class Lot(
   val collectorUser: CollectorUser,
   val fund: Fund
 )
-
-interface Fund {
-  val id: String
-  val name: String
-}
-
-data class CharityFund(
-  override val id: String,
-  override val name: String,
-  val iban: String
-) : Fund
-
-data class MonoBank(
-  override val id: String,
-  override val name: String,
-  val bankNumber: String
-) : Fund
