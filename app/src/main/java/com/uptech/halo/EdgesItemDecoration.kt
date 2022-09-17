ec34704c3948eef9179f78db2e27165d853f41ca
NickHuk <mykola.huk@uptech.team>
2022-09-18 00:31:19 +0300

    item decoration

---
+++ app/src/main/java/com/uptech/halo/EdgesItemDecoration.kt
@@ -0,0 +1,14 @@
+package com.uptech.halo
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
+import androidx.recyclerview.widget.RecyclerView.State
+
+class EdgesItemDecoration : ItemDecoration() {
+
+  override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: State) {
+    super.getItemOffsets(outRect, view, parent, state)
+  }
+}
---
+++ app/src/main/java/com/uptech/halo/Extensions.kt
@@ -0,0 +1,2 @@
+package com.uptech.halo
+
