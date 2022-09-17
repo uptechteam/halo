b712ee16dbc4431592e24c0a0791b37caf1eeb00
NickHuk <mykola.huk@uptech.team>
2022-09-17 22:33:40 +0300

    donation feed

--- app/src/main/java/com/uptech/halo/DonationFragment.kt
+++ app/src/main/java/com/uptech/halo/DonationFragment.kt
@@ -1,7 +1,18 @@
 package com.uptech.halo
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class DonationFragment : Fragment() {
 
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+
+  }
 }
