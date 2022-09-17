dc90bf585a0068c6aac33cabd78d1fed4977c80b
Rodion Kushch <rodionkushch@Rodions-MacBook-Pro.local>
2022-09-17 16:47:09 +0300

    add reward shopitem wallet models

---
+++ app/src/main/java/com/uptech/halo/models/ShopItem.kt
@@ -0,0 +1,20 @@
+package com.uptech.halo.models
+
+interface ShopItem {
+  val id: String
+  val title: String
+  val description: String
+  val price: Long
+  val author: User
+  val reward: Reward
+}
+
+data class ServiceShopItem(
+  override val id: String,
+  override val title: String,
+  override val description: String,
+  override val price: Long,
+  override val author: PartnerUser,
+  override val reward: Reward,
+) : ShopItem
+
