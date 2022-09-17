3662b3768a94266101ec36eb385a5a02c462e45b
NickHuk <mykola.huk@uptech.team>
2022-09-18 01:04:43 +0300

    add margins to donation card

--- app/src/main/java/com/uptech/halo/EdgesItemDecoration.kt
+++ app/src/main/java/com/uptech/halo/EdgesItemDecoration.kt
@@ -10,5 +10,12 @@ class EdgesItemDecoration : ItemDecoration() {
 
   override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: State) {
     super.getItemOffsets(outRect, view, parent, state)
+    outRect.right = outRect.right + 20f.dp().toInt()
+    outRect.left = outRect.left - 20f.dp().toInt()
+
+    when(parent.getChildAdapterPosition(view)) {
+      0 -> outRect.top = outRect.top + 20f.dp().toInt()
+      parent.adapter?.itemCount -> outRect.bottom - 20f.dp().toInt()
+    }
   }
 }
--- app/src/main/java/com/uptech/halo/donation/DonationFragment.kt
+++ app/src/main/java/com/uptech/halo/donation/DonationFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.uptech.halo.EdgesItemDecoration
 import com.uptech.halo.databinding.DonationsFragmentBinding
 
 class DonationFragment : Fragment() {
