3cb0957bfe5a89e2118e0b42c4b056c095b07121
NickHuk <mykola.huk@uptech.team>
2022-09-17 21:51:36 +0300

    login flow

--- app/src/main/java/com/uptech/halo/LoginActivity.kt
+++ app/src/main/java/com/uptech/halo/LoginActivity.kt
@@ -1,11 +1,41 @@
 package com.uptech.halo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions.Builder
+import com.google.android.gms.common.api.ApiException
+import com.uptech.halo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityLoginBinding
+  private val googleSignIn = registerForActivityResult(StartActivityForResult()) { result ->
+    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+    try {
+      val account = task.getResult(ApiException::class.java)
+      finish()
+    } catch (e: ApiException) {
+      println("error")
+    }
+  }
+  private lateinit var googleSignInClient: GoogleSignInClient
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_login)
+    ActivityLoginBinding.inflate(layoutInflater)
+      .also { binding ->
+        this.binding = binding
+        setContentView(binding.root)
+      }
+    binding.signIn.setOnClickListener {
+      val gso = Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .requestEmail()
+        .build()
+      googleSignInClient = GoogleSignIn.getClient(this, gso)
+      googleSignIn.launch(googleSignInClient.signInIntent)
+    }
   }
 }
--- app/src/main/java/com/uptech/halo/MainActivity.kt
+++ app/src/main/java/com/uptech/halo/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.uptech.halo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
 
 class MainActivity : AppCompatActivity() {
+  private val login = registerForActivityResult(StartActivityForResult()) { _ -> }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+  }
+
+  override fun onStart() {
+    super.onStart()
+    val account = GoogleSignIn.getLastSignedInAccount(this)
+    if(account === null) {
+      login.launch(Intent(this, LoginActivity::class.java))
+    } else {
+
+    }
   }
 }
