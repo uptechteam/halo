eef14df7b2e2cd930c383921001772c8cbfd4332
NickHuk <mykola.huk@uptech.team>
2022-09-18 10:22:07 +0300

    fund in donate details

--- app/src/main/java/com/uptech/halo/MainActivity.kt
+++ app/src/main/java/com/uptech/halo/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.gms.auth.api.signin.GoogleSignIn
